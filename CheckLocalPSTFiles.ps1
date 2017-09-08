64b22e103919e330ec74588da67bd71b71941f4a
Chris Rowson <christopherrowson@gmail.com>
2017-09-08 16:49:02 +0100

    Licence

--- CheckLocalPSTFiles.ps1
+++ CheckLocalPSTFiles.ps1
@@ -1,4 +1,20 @@
-﻿$destDir = "\\YOUR_SERVER_HERE\" 
+<# 
+Copyright (C) 2017 Chris Rowson - christopherrowson@gmail.com
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#>
+
+$destDir = "\\YOUR_SERVER_HERE\" 
 $hostName = [System.Net.Dns]::GetHostName()
 $userName = [Environment]::UserName
 $fullPath = $destDir + $hostName + "-" + $userName + ".txt"
